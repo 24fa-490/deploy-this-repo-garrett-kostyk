aeb4ef59efc2591110e74028d0655cfe5fd6614f
garrett-kostyk <112421263+garrett-kostyk@users.noreply.github.com>
2024-10-27 21:27:30 -0700

    Update database.js

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,9 +1,4 @@
 
 import postgres from 'postgres';
-
-// see env variables in .env
-import {PGCONNECT} from '$env/static/private';
-
-const sql = postgres(process.env.PGCONNECT, {} )
-
+const sql = postgres(process.env.PGCONNECT, {});
 export default sql;
